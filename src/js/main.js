52687d6621ca247462080f5e7deccae8a55e85a4
Maria Jose Serrano Higueras <mariajoseserranohigueras@MacBook-Air-de-Maria-Jose.local>
2025-10-27 12:59:17 +0100

    toggle added

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,33 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+console.log(">> Ready :)");
+
+// QUERY SELECTOR:
+// hemos llamado al evento felchas
+const arrow1 = document.querySelector(".js_arrow1");
+const arrow2 = document.querySelector(".js_arrow2");
+const arrow3 = document.querySelector(".js_arrow3");
+
+// hemos llamado al contenido que le afecta ese evento
+
+const colorArticle = document.querySelector(".js_colorArticle");
+const fillArticle = document.querySelector(".js_fillArticle");
+const shareArticle = document.querySelector(".js_shareArticle");
+
+// EVENTOS
+
+arrow1.addEventListener("click", (ev) => {
+  console.log("Holis!");
+  //hemos ocultado la primera seccion
+  colorArticle.classList.toggle("collapsed");
+});
+
+arrow2.addEventListener("click", (ev) => {
+  //hemos ocultado la primera seccion
+  fillArticle.classList.toggle("collapsed");
+});
+
+arrow3.addEventListener("click", (ev) => {
+  //hemos ocultado la primera seccion
+  shareArticle.classList.toggle("collapsed");
+});
