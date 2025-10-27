93b09aba8f4906add8131e3e9067bb25df329f9e
Eilith404 <minaya.sofia@gmail.com>
2025-10-27 13:29:37 +0100

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@
 console.log(">> Ready :)");
 
 // QUERY SELECTOR:
-// hemos llamado al evento felchas
+// hemos llamado al evento flechas
 const arrow1 = document.querySelector(".js_arrow1");
 const arrow2 = document.querySelector(".js_arrow2");
 const arrow3 = document.querySelector(".js_arrow3");
@@ -14,7 +14,14 @@ const colorArticle = document.querySelector(".js_colorArticle");
 const fillArticle = document.querySelector(".js_fillArticle");
 const shareArticle = document.querySelector(".js_shareArticle");
 
+// llamar al preview
+
+const card = document.querySelector(".js_cardPreview"); // la tarjeta de preview que va a ir cambiando
+const paletteRadios = document.querySelectorAll('input[name="palette"]'); // devuelve todos los radios que coinciden
+
 // EVENTOS
+
+//1.Eventos para toggle
 
 arrow1.addEventListener("click", (ev) => {
   console.log("Holis!");
@@ -30,4 +37,24 @@ arrow2.addEventListener("click", (ev) => {
 arrow3.addEventListener("click", (ev) => {
   //hemos ocultado la primera seccion
   shareArticle.classList.toggle("collapsed");
+});
+
+//2.Preview colores
+
+// Función con condicionales
+function applyPalette(value) {
+  card.classList.remove("palette-1", "palette-2", "palette-3"); // quitamos paletas anteriores
+  if (value === "1") {
+    card.classList.add("palette-1");
+  } else if (value === "2") {
+    card.classList.add("palette-2");
+  } else if (value === "3") {
+    card.classList.add("palette-3");
+  }
+}
+
+paletteRadios.forEach((radio) => {
+  radio.addEventListener("click", function () {
+    applyPalette(this.value); // this.value es '1' | '2' | '3'
+  });
 });
